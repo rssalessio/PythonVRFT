86ca5b2eda69fc5384482d6c119278f4a97e5ff9
Alessio Russo <alessior@kth.se>
2021-01-06 22:50:26 +0100

    add license header to each file

--- test/test_vrft.py
+++ test/test_vrft.py
@@ -1,3 +1,19 @@
+# Copyright [2017-2020] [Alessio Russo - alessior@kth.se]  
+# This file is part of PythonVRFT.
+# PythonVRFT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# PythonVRFT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with PythonVRFT.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Code author: [Alessio Russo - alessior@kth.se]
+# Last update: 06th January 2020, by alessior@kth.se
+#
+
 from unittest import TestCase
 from vrft.iddata import *
 from vrft.vrft_algo import *
--- vrft/utils.py
+++ vrft/utils.py
@@ -1,6 +1,22 @@
+# Copyright [2017-2020] [Alessio Russo - alessior@kth.se]  
+# This file is part of PythonVRFT.
+# PythonVRFT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# PythonVRFT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with PythonVRFT.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Code author: [Alessio Russo - alessior@kth.se]
+# Last update: 06th January 2020, by alessior@kth.se
+#
+
 import numpy as np
 import scipy.signal as scipysig
-from .iddata import iddata
+from vrft.iddata import iddata
 
 def Doperator(p: int, q: int, x: float) -> np.ndarray:
     D = np.zeros((p * q, q))
